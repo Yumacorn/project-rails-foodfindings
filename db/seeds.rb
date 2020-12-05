d62097189108ed6aa2d04980ef8d883395d71c96
Yumacorn <ReachRyanMui@gmail.com>
2020-12-05 08:20:25 -0500

    removed the reference key column for user. Does not need

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: "Ryan", username: "Yumacorn", city: "Selden", state: "NY")
+#Possibly add a reference key? shouldn't need it
